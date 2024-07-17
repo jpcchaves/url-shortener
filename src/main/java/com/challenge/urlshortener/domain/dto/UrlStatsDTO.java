5bb71ee9eff32ea4f5de299c26e4f414a85d514d
Joao Chaves <joao.chaves@liferay.com>
2024-07-17 09:17:23 -0300

    add url stats dto

--- src/main/java/com/challenge/urlshortener/domain/dto/UrlStatsDTO.java
+++ src/main/java/com/challenge/urlshortener/domain/dto/UrlStatsDTO.java
@@ -3,4 +3,31 @@ package com.challenge.urlshortener.domain.dto;
 public class UrlStatsDTO {
   private Integer accessCount;
   private Double averageAccessPerDay;
+
+  public UrlStatsDTO() {
+  }
+
+  public UrlStatsDTO(
+      Integer accessCount,
+      Double averageAccessPerDay
+  ) {
+    this.accessCount = accessCount;
+    this.averageAccessPerDay = averageAccessPerDay;
+  }
+
+  public Integer getAccessCount() {
+    return accessCount;
+  }
+
+  public void setAccessCount(Integer accessCount) {
+    this.accessCount = accessCount;
+  }
+
+  public Double getAverageAccessPerDay() {
+    return averageAccessPerDay;
+  }
+
+  public void setAverageAccessPerDay(Double averageAccessPerDay) {
+    this.averageAccessPerDay = averageAccessPerDay;
+  }
 }
